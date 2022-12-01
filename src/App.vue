a9733b12dbc18386abc2bb5018e039d2cf0f406b
Mehmet Can Aygün <mehmetcanaygun@gmail.com>
2022-12-01 12:14:22 +0300

    Add new font & update container class

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <RouterView />
+  <div class="w-full min-h-screen overflow-x-hidden font-Nunito">
+    <RouterView />
+  </div>
 </template>
 
 <style scoped></style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,30 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      Nunito: ["Nunito, sans-serif"],
+    },
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "0",
+      },
+      center: true,
+      screens: {
+        sm: "600px",
+        md: "728px",
+        lg: "984px",
+        xl: "1240px",
+        "2xl": "1496px",
+      },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "984px",
+      xl: "1240px",
+      "2xl": "1496px",
+    },
   },
   plugins: [],
 };
